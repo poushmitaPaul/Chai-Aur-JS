a50b51e3c4454d64daa2b7a1e169e56ecd4ee251
Poushmita Paul <161232111+poushmitaPaul@users.noreply.github.com>
2024-08-31 08:13:35 +0000

    datatypes

---
+++ 01_Basics/2_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict";  // treat all JS Code as newer version.
+
+
+
+
+// alert(3+3)  // we are using nodejs, not browser. 
+console.log(3
+    +
+    3
+)
+console.log(3+3); // Code readability should be high.
+// tc39.es and mdn  // documentation
+
+
+
+let name = "Ria"
+let age = 10 
+let isLoggedIn = true
+let state;
+/* 
+number => 2 to the power 52/53.
+bigInt => big number. 
+*/
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not asigned. 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // output --> undefined
+console.log(typeof null); // output --> object
+
