bd512569e9ad4553ce66a4786d4490013682b404
Poushmita Paul <161232111+poushmitaPaul@users.noreply.github.com>
2024-09-01 09:28:59 +0000

    conversion of datatypes

---
+++ 01_Basics/3_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "33abc"
+let num = null
+let age = undefined
+let adult = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber1 = Number(score)
+// console.log(typeof valueInNumber1); //number
+// console.log(valueInNumber1);  //NaN
+
+
+// let valueInNumber2 = Number(num)
+// console.log(typeof num);
+// console.log(num);
+// console.log(typeof valueInNumber2);
+// console.log(valueInNumber2);
+
+
+// let valueInNumber3 = Number(age)
+// console.log(typeof age);
+// console.log(age);
+// console.log(typeof valueInNumber3);
+// console.log(valueInNumber3);
+
+
+let valueInNumber4 = Number(adult)
+console.log(typeof adult);
+console.log(adult);
+console.log(typeof valueInNumber4);
+console.log(valueInNumber4);
+
+
