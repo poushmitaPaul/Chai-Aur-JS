49839bc16c395347b6dde5dacdda5738ca984b63
Poushmita Paul <161232111+poushmitaPaul@users.noreply.github.com>
2024-09-05 08:58:19 +0000

    conversion and comparsion

--- 01_Basics/3_conversionOperation.js
+++ 01_Basics/3_conversionOperation.js
@@ -32,9 +32,9 @@
 // console.log(typeof valueInNumber4);
 // console.log(valueInNumber4);
 
-let loggedIn = 1
-let booleanIsLoggedIn = Boolean(loggedIn)
-console.log(booleanIsLoggedIn);
+// let loggedIn = 1
+// let booleanIsLoggedIn = Boolean(loggedIn)
+// console.log(booleanIsLoggedIn);
 
 
 
@@ -43,3 +43,52 @@ console.log(booleanIsLoggedIn);
 "33abc" => NaN
 true => 1 , false => 0
 */
+
+
+
+// *************** OPERATIONS ************** //
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(3%2);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 4 * 5 % 3);
+// console.log((3 + 4) * (5 % 3));
+
+
+console.log(true+);
+console.log(+true);
+console.log(+"");
+
+
+let num1, num2, num3 
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+gameCounter++;
+console.log(gameCounter);
+
+// link to study
+/* 
+https://tc39.es/ecma262/multipage/adstract-operations.html#sec-type-conversion
+*/
+
