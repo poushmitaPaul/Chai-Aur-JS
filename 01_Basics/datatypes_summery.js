1db8dd5f886c3058d639872cc590c027c9a4a782
Poushmita Paul <161232111+poushmitaPaul@users.noreply.github.com>
2024-09-10 15:19:24 +0000

    dataType_summary

---
+++ 01_Basics/datatypes_summery.js
@@ -0,0 +1,11 @@
+// Primitive - (Call By Value) //
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt.
+
+
+// JavaScript is a Dynamically typed language, because data type will automatically assigned at the time of compilation or code execution. (just in time compilation language) //
+
+
+// Non Primitive - (Call By Refference) //
+
+// Array, Objects, Functions
